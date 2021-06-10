be6e0c52a1223c71b2eae2d42c848fe076ed9dc3
Nicolas <Nicolas Rousset>
2021-06-10 11:44:28 +0200

    WiP

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
-const routes = require('./routes/routes');
 const path = require('path');
-const app = express(); // const app = express.createWebServerApplication()
+const http = require('http');
+
+const {Server} = require('socket.io');
+
+const chatApp = require('./services/chatApp');
+const routes = require('./routes/routes');
+
 const PORT = 3000;
+
+const app = express(); // const app = express.createWebServerApplication()
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.set('view engine', hbs);
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, '/public')));
+chatApp.handleSocketIo(io);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`app is running on PORT ${PORT}`);
 });
 module.exports = app;
---
+++ services/chatApp.js
@@ -0,0 +1,45 @@
+const YODA_CITATIONS = [
+  'Difficile à voir. Toujours en mouvement est l’avenir.',
+  'Le côté obscur de la Force, redouter tu dois.',
+  'Fais le ou ne le fais pas. Il n’y a pas d’essai.',
+  'Bonnes relations avec les Wookies, j’entretiens.',
+  'Un grand guerrier ? Personne par la guerre ne devient grand.',
+  'Un Jedi utilise la Force pour la connaissance et la défense,' +
+    ' jamais pour l’attaque.',
+  'Quand 900 ans comme moi tu auras, moins en forme tu seras.',
+  'Je ne peux rien lui apprendre, cet enfant n’a aucune patience.' +
+    ' Trop de colère en lui, comme son père, il n’est pas prêt.',
+  'Robuste je suis grâce à la Force, mais pas à ce point là.' +
+    ' Le crépuscule m’envahi et bientôt, la nuit va tomber. Humm.' +
+    ' Ainsi vont les choses. Ainsi va la Force.',
+  'La peur est le chemin vers le côté obscur : la peur mène à la colère,' +
+    ' la colère mène à la haine, la haine … mène à la souffrance.',
+  'Toujours par deux ils vont. Ni plus, ni moins. Le maître et son apprenti.',
+  'A vos intuitions vous fier, il faut.',
+  'La taille importe peu … Regarde moi : est-ce par la taille que tu peux' +
+    ' me juger ? Et bien tu ne le dois pas.',
+];
+
+/** Renvoit une citation aléatoire de yoda
+ * @return {string} une citation aléatoire de yoda
+ */
+function getYodaCitation() {
+  return YODA_CITATIONS[Math.floor((Math.random()*YODA_CITATIONS.length))];
+}
+
+exports.handleSocketIo = (io) => {
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      socket.emit('Yoda message', {message: getYodaCitation()});
+    });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+};
+
+
